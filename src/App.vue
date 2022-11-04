9b7f9fd7ec21041627cc50757d9d82fafcca7e71
Barneyek <osieckidamian1992@gmail.com>
2022-11-04 20:49:17 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      city: "Warsaw",
+    };
+  },
+  created() {
+    console.log("test")
+    this.getCurrentWeather();
+  },
+  methods: {
+    getCurrentWeather() {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${this.APIKey}`)
+        .then(res => {
+          console.log(res.data);
+        });
+    },
+  },
+};
+</script>
