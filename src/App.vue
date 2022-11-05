361758781d70c4c7163862638a8ceff9dc2e45cd
Barneyek <osieckidamian1992@gmail.com>
2022-11-05 12:49:17 +0100

    Import db and update weather data of city

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 <script>
 import axios from "axios";
+import db from "./firebase/firebaseInit";
 
 export default {
   name: "App",
@@ -12,12 +13,44 @@ export default {
     return {
       city: "Warsaw",
       API_Key: import.meta.env.VITE_APP_WEATHER_KEY,
+      cities: [],
     };
   },
   created() {
-    this.getCurrentWeather();
+    // this.getCurrentWeather();
+    this.getCityWeather();
   },
   methods: {
+    getCityWeather() {
+      let firebaseDB = db.collection("cities");
+      firebaseDB.onSnapshot((snap) => {
+        snap.docChanges().forEach(async (doc) => {
+          if (doc.type === "added") {
+            try {
+              const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${
+                  doc.doc.data().city
+                }&appid=${this.API_Key}`
+              );
+              const data = response.data;
+              firebaseDB
+                .doc(doc.doc.id)
+                .update({
+                  currentWeather: data,
+                })
+                .then(() => {
+                  this.cities.push(doc.doc.data());
+                })
+                .then(() => {
+                  console.log(this.cities);
+                });
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        });
+      });
+    },
     getCurrentWeather() {
       axios
         .get(
